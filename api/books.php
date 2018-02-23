f3e6cb3eb888f4448814266c47d0a29a6f8323bc
wieckowski.praca@gmail.com <wieckowski.praca@gmail.com>
2018-02-23 22:38:33 +0100

    Poprawienie ajaxów do wyświetlania, tworzenia, usuwania książek, poprawa obslugi od strony servera

--- api/books.php
+++ api/books.php
@@ -5,14 +5,14 @@ require __DIR__ . "/src/Book.php";
 
 if($_SERVER["REQUEST_METHOD"]=="POST")
 {
-     $name = $_POST['postname'];
-     $author = $_POST['postauthor'];
-     $description = $_POST['postdescription'];
-     $newBook = new Book;
-     $newBook->create($conn, $name, $author, $description);
-     $booksArr = Book::loadAllFromDB($conn);
-     $serializedBooksArr =  json_encode($booksArr);
-     echo $serializedBooksArr;//ajax wyłapuje tylko to co się wyświetla na stronie
+  $name = $_POST['name'];
+  $author = $_POST['author'];
+  $description = $_POST['description'];
+  $newBook = new Book;
+  $newBook->create($conn, $name, $author, $description);
+  $booksArr = Book::loadAllFromDB($conn);
+  $serializedBooksArr =  json_encode($booksArr);
+  echo $serializedBooksArr;//ajax wyłapuje tylko to co się wyświetla na stronie
 }
 
 if($_SERVER["REQUEST_METHOD"]=="GET")
@@ -22,15 +22,22 @@ if($_SERVER["REQUEST_METHOD"]=="GET")
      $id = $_GET['id'];
      $book = Book::loadFromDB($conn, $id);
      $serializedBook = json_encode($book);
-     //var_dump($serializedBook);//gdyby ten var_dump nie był zakomentowany to ajax rzucał by błedem parseerror
-     echo $serializedBook;//ajax wyłapuje tylko to co się wyświetla na stronie
-     //echo $book->getName();
+     echo $serializedBook;
   }
   else
   {
     $booksArr = Book::loadAllFromDB($conn);
     $serializedBooksArr =  json_encode($booksArr);
-    echo $serializedBooksArr;//ajax wyłapuje tylko to co się wyświetla na stronie
+    echo $serializedBooksArr;
   }
+}
+
+if($_SERVER['REQUEST_METHOD'] == "DELETE")
+{
+  $id = $_GET['id'];
+  book::staticDeleteFromDB($conn, $id);
+  $booksArr = Book::loadAllFromDB($conn);
+  $serializedBooksArr =  json_encode($booksArr);
+  echo $serializedBooksArr;
 }
 ?>
--- js/app.js
+++ js/app.js
@@ -1,75 +1,103 @@
 $(function()
 {
-  $.getJSON
+  // main variables--------------------------------------------------------
+
+  var booksDiv = $("#booksList");
+  var mainForm = $("#mainForm");
+
+  //main ajax--------------------------------------------------------------
+
+  $.ajax
   ({
-    url:"http://localhost/Powtorka_Warszt3_Library/api/books.php"
+    url:"http://localhost/Powtorka_Warszt3_Library/api/books.php",
+    type: "GET",
+    dataType: "JSON"
   })
   .done(function(response)//response jak event w callbacku
   {
-    console.log("udało się! :)");
-    console.log(response);
-    var books = response;
-    allBooks(books);
+    allBooks(response);
   })
-  .fail(function(jqXHR, textStatus) {alert("Request failed: " + textStatus);})
-  .always(function(){console.log("jeśli się udało to super, a jak nie to nic nie szkodzi, w koncu się uda")});
+  .fail(function(jqXHR, textStatus) {alert("Request failed: " + textStatus);});
+
+  //allBooksFunction - showing all books names--------------------------------
 
   function allBooks(books)
   {
     for (var book of books)
     {
       var id = book.id;
-      var div = $("<div class='name' data-id=" + id + ">" + book.name + "<div class='info'></div></div>");
-      $("#booksList").append(div);
-      div.click(showInfo)//gdyby było click(showInfo()) to jako callback zostal by przekazany od razu wynik dzialania funkcji a nie funkcja jako taka.
+      var name = book.name;
+      var div = $("<div class='name' data-id="+id+">" + name + "<div class='info'></div></div>");
+      var button = $("<button data-id="+id+">usuń</button>");
+
+      booksDiv.append(div);
+      div.click(showInfo);
+      div.after(button);
+      button.click(deleteBook);
     }
   }
 
-  function showInfo()
-  {
-    var id = this.dataset.id; //lub var id = $(this).data("id");
+  //deleteBookFunction--------------------------------------------------------
 
-    $.getJSON
+  function deleteBook()
+  {
+    var id = this.dataset.id;
+    
+    $.ajax
     ({
-      url:"http://localhost/Powtorka_Warszt3_Library/api/books.php?id="+id
+      url: "http://localhost/Powtorka_Warszt3_Library/api/books.php?id="+id,
+      type: "DELETE",
+      dataType: "JSON"
     })
-    .done(function(response)//response jak event w callbacku
+    .done(function(response)
     {
-      console.log("znów się udało! :)");
-      var book = response;
-      var infoDiv = $("div[data-id="+id+"]").find("div.info");
-      infoDiv.text("author: "+ book.author +" description: "+book.description);
+       booksDiv.empty();
+       mainForm[0].reset();
+       allBooks(response);
     })
-    .fail(function(jqXHR, textStatus) {alert("Request failed: " + textStatus);})
-    .always(function(){console.log("jeśli się udało to super, a jak nie to nic nie szkodzi, w koncu się uda")});
+    .fail(function(jqXHR, textStatus) {alert("Request failed: " + textStatus);});
   }
 
-  $("#mainForm").on("submit", function(event)
+  //showInfoFunction - showing info DIV about single book----------------------
+
+  function showInfo()
+  {
+    var id = this.dataset.id;
+
+    $.ajax
+    ({
+      url:"http://localhost/Powtorka_Warszt3_Library/api/books.php?id="+id+"",
+      type: "GET",
+      dataType: "JSON"
+    })
+    .done(function(response)
+    {
+      var author = response.author;
+      var description = response.description;
+      var infoDiv = $("div[data-id="+id+"]").find("div.info");
+      infoDiv.text("autor: "+ author +" opis: "+ description);
+    })
+    .fail(function(jqXHR, textStatus) {alert("Request failed: " + textStatus);});
+  }
+
+  //addingBookByForm-----------------------------------------------------------------
+
+  mainForm.on("submit", function(event)
   {
     event.preventDefault();
 
-    // var form = $(event.target);
-    // console.log(form.attr("action"));
-    //
-    // $.ajax({
-    //   url:  form.attr("action"),
-    //   type: "POST",
-    //   data: form.serialize(),
-    //   dataType: "json"
-    // })
-
-    var name = $("#inputName").val();
-    var author = $("#inputAuthor").val();
-    var description = $("#inputDescription").val();
-
-    $.post('api/books.php',{postname:name, postauthor:author, postdescription:description},function(response)
+    $.ajax
+    ({
+    url:  mainForm.attr("action"),
+    type: "POST",
+    data: mainForm.serialize(),
+    dataType: "JSON"
+    })
+    .done(function(response)
     {
-       console.log("udało się 3! :)");
-       console.log(response);
-       $("#booksList").empty();
-       var books = response;
-       allBooks(books);
-
-    },"json");
+      booksDiv.empty();
+      mainForm[0].reset();
+      allBooks(response);
+    });
   });
 });
